2ded8aa2e5a17d9a784526e5baffcb6a4e8bf6c9
Your Name <you@example.com>
2023-11-28 18:10:38 +0200

    kääntäminen

--- allu3D/Assets/PlayerController.cs
+++ allu3D/Assets/PlayerController.cs
@@ -6,11 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5.5f;
     public CharacterController controller;
+    public float mouseSpeed = 3f;
+    public CursorLockMode cursorLock;
+
+    private float mouseMovementX = 0f;
+    private float mouseMovementY = 0f;
 
     Vector3 moveDir;
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = cursorLock;
+        Cursor.visible = (CursorLockMode.Locked != cursorLock);
         
     }
 
@@ -19,7 +26,16 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        mouseMovementX += Input.GetAxis("Mouse X") * mouseSpeed;
+        mouseMovementY -= Input.GetAxis("Mouse Y") * mouseSpeed;
+
+        Camera.main.transform.localRotation = Quaternion.Euler(mouseMovementX, 0, 0);
+
+        transform.localRotation = Quaternion.Euler(0,mouseMovementX,0);
+
         moveDir = new Vector3(horizontal,0,vertical);
+        moveDir = transform.rotation * moveDir;
 
         controller.SimpleMove(moveDir * moveSpeed);
     }
